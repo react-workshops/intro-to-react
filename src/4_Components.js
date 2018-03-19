18df7453f31d5b41d2cf26c0056568a1144431dd
Mateusz Zatorski <mateuszzatorski@gmail.com>
2018-03-19 13:48:00 +0100

    add back the components content

--- src/4_Components.js
+++ src/4_Components.js
@@ -17,7 +17,79 @@ export default class Components extends React.Component {
     const person = { name: "Lucy" };
     return (
       <ScrollScreen title="React Components" navigation={navigation}>
-        {person && <Person person={person} />}
+        <Card>
+          <Title>Custom Components</Title>
+          <Paragraph>
+            Components can be a simple function, with a props object as the
+            argument:
+          </Paragraph>
+          <Code
+            code={`	
+const Person = (props) => (	
+  <Text>	
+    Your name is {props.name}	
+  </Text>	
+);	
+            `}
+          />
+          <Paragraph>
+            The parent component would render your custom component:
+          </Paragraph>
+          <Code
+            code={`	
+const App = () => (	
+  <Person name="" />	
+);	
+            `}
+          />
+        </Card>
+        <Card>
+          <Title>JSX</Title>
+          <Paragraph>
+            The XMLlike is called JSX, and it allows easy mixing of
+            declarativestyle syntax, with JavaScript, by using the "{}" curly
+            braces
+          </Paragraph>
+          <Code
+            code={`	
+const Person = (props) => (	
+  <Text>	
+    You are	
+    {props.name === 'Jim' ? '' : ' not'}	
+    Jim	
+  </Text>	
+);	
+            `}
+          />
+        </Card>
+        <Card>
+          <Title>Children prop</Title>
+          <Paragraph>
+            The content between the opening and closing tag is known as
+            "children", and it acts like a normal prop.
+          </Paragraph>
+          <Code
+            code={`	
+const BlueText = (props) => (	
+  <Text style={{color: 'blue'}}>	
+    {props.children}	
+  </Text>	
+);	
+            `}
+          />
+          <Paragraph>
+            The parent component can render pass children normally:
+          </Paragraph>
+          <Code
+            code={`	
+const BlueHello = () => (	
+  <BlueText>	
+    Hello!	
+  </BlueText>	
+);	
+            `}
+          />
+        </Card>
       </ScrollScreen>
     );
   }
